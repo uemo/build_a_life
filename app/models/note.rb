3ad4cf9b09f7799188d4a250bad0d5763d4becfa
uemo <uetti_tu@yahoo.co.jp>
2019-06-15 17:02:54 +0900

    [Add]いいね機能実装

--- app/models/note.rb
+++ app/models/note.rb
@@ -2,6 +2,10 @@ class Note < ApplicationRecord
 	belongs_to :user
 	belongs_to :works
 	has_many :comments
-	has_many :favorites
+	has_many :favorites, dependent: :destroy
+
+	def favorited_by?(user)
+          favorites.where(user_id: user.id).exists?
+    end
 
 end
